4c0282ae0568db7d3e5338d2284fa01cbf474f33
Giovanni Napoli <gnapoli.dev@gmail.com>
2022-08-16 12:05:49 +0200

    xml_url validation

--- src/errors.rs
+++ src/errors.rs
@@ -4,13 +4,15 @@ use std::fmt::Display;
 pub enum Error<'a> {
     UnableToParseArg(&'a str), // Generic error to handle every cli args error
     PathDoesNotExist,
+    UnableToParseUrl(&'a str)
 }
 
 impl<'a> Display for Error<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
+        match &self {
             Error::UnableToParseArg(arg) => write!(f, "Unable to parse command line argument: {:?}", arg),
             Error::PathDoesNotExist => write!(f, "File path does not exist"),
+            Error::UnableToParseUrl(arg) => write!(f, "Unable to parse url: {:?}", arg)
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use std::ops::RangeInclusive;
 use std::path::Path;
+use url::Url;
 
 use clap::{AppSettings, Parser};
 mod errors;
 use errors::Error;
-
 
 const THREADS_NUM_RANGE: RangeInclusive<i64> = 1..=10;
 const CALLS_NUM_RANGE: RangeInclusive<i64> = 1..=1000;
@@ -22,7 +22,7 @@ pub struct CmdArgs {
     #[clap(short = 'd', long, value_name = "FILE.txt", value_parser = parse_dictionary_path)]
     pub dictionary: String,
     /// XML RPC API url for the attack [required]
-    #[clap(short = 'x', long)]
+    #[clap(short = 'x', long, value_parser = parse_xml_url)]
     pub xml_url: String,
     /// Number of threads to run [optional, (min 1 - max 10) default=5]
     #[clap(short = 't', long, value_parser = clap::value_parser!(u8).range(THREADS_NUM_RANGE))]
@@ -35,19 +35,27 @@ pub struct CmdArgs {
     pub verbose_mode: bool,
 }
 
-fn parse_dictionary_path(path_arg: &str) -> Result<String, String>{
-    Path::new(path_arg).try_exists().map(|exst| {
-        if exst {
-            Ok(path_arg.into())
-        } else {
-            Err(format!("Path '{}' does not exist", path_arg))
-        }
-    }).map_err(|_| Error::UnableToParseArg(path_arg))?
+fn parse_dictionary_path(dictionary_arg: &str) -> Result<String, String> {
+    Path::new(dictionary_arg)
+        .try_exists()
+        .map(|exst| {
+            if exst {
+                Ok(dictionary_arg.into())
+            } else {
+                Err(format!("Path '{}' does not exist", dictionary_arg))
+            }
+        })
+        .map_err(|_| Error::UnableToParseArg(dictionary_arg))?
+}
+
+fn parse_xml_url(xml_url_arg: &str) -> Result<String, String> {
+    Url::parse(xml_url_arg)
+        .map(|url| Ok(url.into()))
+        .map_err(|_| Error::UnableToParseUrl(xml_url_arg))?
 }
 
 pub fn main() {
     let args = CmdArgs::parse();
 
     println!("{:?}", args);
-
 }
